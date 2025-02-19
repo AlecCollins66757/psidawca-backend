6a8069c44b67c68e10c5a1eb666e9841de112e0e
AlecCollins66757 <aleccollins66@gmail.com>
2025-02-19 16:14:24 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,9 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL") + "?sslmode=require"  # Dodaj SSL
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
