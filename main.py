c14fbfa0a9d9da6b42c2f1f5267bffbcb86114b1
AlecCollins66757 <aleccollins66@gmail.com>
2025-02-19 10:17:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,19 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+# Montowanie plików statycznych (CSS, JS)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Inicjalizacja systemu szablonów
+templates = Jinja2Templates(directory="templates")
+
+# Endpoint do renderowania formularza rejestracji psa
+@app.get("/register_dog")
+async def register_dog(request: Request):
+    return templates.TemplateResponse("register_dog.html", {"request": request})
